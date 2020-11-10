999d1015fe0daacd1492b68baacefcc6bec241a1
LaurenBugelli <70624106+LaurenBugelli@users.noreply.github.com>
2020-11-10 11:51:46 -0800

    Add files via upload

---
+++ currProjFolder/currProj.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <meta charset = "UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel = "stylesheet" href="http://laurenbugelli.com/Css/currProjStyle.css">
+    <title> Lauren's Current Project Page </title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <h3 id="logo">Current Project</h3>
+        <div class="hamburger">
+          <a style=text-decoration:none href="http://laurenbugelli.com">
+            <h1>Home</h1>
+          </a>
+        </div>
+      </nav>
+      <section class ="top">
+        <div class="hero">
+          <img src="http://laurenbugelli.com/Imgs/homePhoto.jpg" alt="">
+          <h1 class="headline">Project Title</h1>
+        </div>
+      </section>
+    </header>
+
+    <div class="slider"></div>
+    <div class = "contact">
+      <a style=text-decoration:none href="http://laurenbugelli.com/ContactMe/contactMe.php">
+        <h1 class>Like What You See?</h1>
+      </a>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineMax.min.js"
+      integrity="sha512-lJDBw/vKlGO8aIZB8/6CY4lV+EMAL3qzViHid6wXjH/uDrqUl+uvfCROHXAEL0T/bgdAQHSuE68vRlcFHUdrUw=="
+      crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"
+      integrity="sha512-8Wy4KH0O+AuzjMm1w5QfZ5j5/y8Q/kcUktK9mPUVaUoBvh3QPUZB822W/vy7ULqri3yR8daH3F58+Y8Z08qzeg=="
+      crossorigin="anonymous">
+    </script>
+    <script src="http://laurenbugelli.com/js/currProjJava.js"></script>
+  </body>
+</html>
