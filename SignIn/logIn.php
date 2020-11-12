53a6704ae73b41dda9984fa82c3af78292a6e4a1
LaurenBugelli <70624106+LaurenBugelli@users.noreply.github.com>
2020-11-11 21:52:22 -0800

    update login div class tags

--- SignIn/logIn.php
+++ SignIn/logIn.php
@@ -1,30 +1,42 @@
+<?php
+  session_start();
+ ?>
 <!DOCTYPE html>
 <html lang = "en">
 <head>
   <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
   <meta charset="UTF-8">
   <title>Lauren Bugelli</title>
-  <link rel = "stylesheet" href="http://laurenbugelli.com/Css/homeStyle.css">
+  <link rel = "stylesheet" href="http://laurenbugelli.com/Css/signInStyle.css">
 </head>
 <body>
   <section class = "signup-form">
-    <h2>Login</h2>
-    <div class="signUp-style">
-      <form action="http://laurenbugelli.com/php/login-inc.php" method="post">
-        <input type="text" name="uid" placeholder="Username/Email...">
-        <input type="password" name="pass" placeholder="Password...">
-        <button type="submit" name="submit">Login</button>
-      </form>
+    <div class = "signin-wrapper">
+      <div class="signUp-style">
+        <form action="http://laurenbugelli.com/php/login-inc.php" method="post" class = "form" autocomplete="off">
+          <h2>Login</h2>
+          <div class="input-group">
+            <input type="text" name="uid">
+            <label for="name">Display name/Email...</label>
+          </div>
+          <div class = "input-group">
+            <input type="password" name="pass">
+            <label for="pass">Password...</label>
+          </div>
+          <?php
+            if(isset($_GET["error"])){
+              if($_GET["error"] == "emptyinput"){
+                echo "<p>Fill in all fields!</p>";
+              }else if($_GET["error"] == "wronglogin"){
+                echo "<p>Incorrect login information</p>";
+              }
+            }
+           ?>
+          <button type="submit" name="submit" class = "submit-btn">Login</button>
+          <a style="text-decoration:none" href ="/index.php" class = "home-btn"> Return Home</a>
+        </form>
+      </div>
     </div>
-    <?php
-      if(isset($_GET["error"])){
-        if($_GET["error"] == "emptyinput"){
-          echo "<p>Fill in all fields!</p>";
-        }else if($_GET["error"] == "wronglogin"){
-          echo "<p>Incorrect login information</p>";
-        }
-      }
-     ?>
   </section>
     <?php
       include_once 'footer.php';
