ff463b0ae12c339f6784766510d55f5e44b86e74
LaurenBugelli <70624106+LaurenBugelli@users.noreply.github.com>
2020-11-11 21:50:31 -0800

    redo footer layout

--- Headers-Footers/footer.php
+++ Headers-Footers/footer.php
@@ -1,7 +1,38 @@
 <!-- FOOTER -->
-<section class = "footer">
-  <p class = "text">Thank you for checking out my website!</p>
-</section>
+<footer>
+  <div class = "footer-content">
+    <h3>Lauren Bugelli</h3>
+    <p>Thank you for visiting my Artist Page!</p>
+    <ul class = "footSocials">
+      <div class="footIcons">
+        <div class = "footIconBx">
+          <a style=text-decoration:none href="http://instagram.com">
+            <img src = "http://laurenbugelli.com/Imgs/insta.png">
+          </a>
+        </div>
+        <div class = "footIconBx">
+          <a style=text-decoration:none href="http://facebook.com">
+            <img src = "http://laurenbugelli.com/Imgs/facebook.png">
+          </a>
+        </div>
+        <div class = "footIconBx">
+          <a style=text-decoration:none href="http://linkedin.com">
+            <img src = "http://laurenbugelli.com/Imgs/linkIn.png">
+          </a>
+        </div>
+        <div class = "footIconBx">
+          <a style=text-decoration:none href=#top>
+            <img src = "http://laurenbugelli.com/Imgs/home.png">
+          </a>
+        </div>
+      </div>
+    </ul>
+  </div>
+  <div class="footer-bottom">
+    <p>Created as a student project. Fall 2020</p>
+  </div>
+</footer>
+
   <script src="http://laurenbugelli.com/js/homeJavaS.js"></script>
 </body>
 </html>
